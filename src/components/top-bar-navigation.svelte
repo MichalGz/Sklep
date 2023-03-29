679d6386c65b1fbd76717633d97a72648cc9ac6b
Michal Grzebinoga <m.grzebinoga24@gmail.com>
2023-03-29 12:57:04 +0200

    it had been TopBarNavigation done

--- src/components/top-bar-navigation.svelte
+++ src/components/top-bar-navigation.svelte
@@ -1,1 +1,19 @@
-todo
+<nav class="navigation">
+<a class="navi" href="carrer.html">carrers</a>
+<a class="navi" href="help.html">help</a>
+<a class="navi" href="buyer-protection.html">Buyer protection</a>
+</nav>
+
+<style>
+    .navi {
+        padding: 16px;
+        color: white;
+        text-decoration: none;
+    }
+    a:hover {
+        color: red;
+    }
+    .navigation {
+        margin-right: 500px;
+    }
+</style>
